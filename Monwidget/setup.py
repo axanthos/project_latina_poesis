a3c507f9e37aec5d5f75720b4880829efe75ba3a
Jandenm <julien.andenmatten@unil.ch>
2016-05-31 17:12:35 +0200

    Delete setup.py

--- Monwidget/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="Latina_Poesis",
-    packages = ["Textableprototype"],
-    entry_points = {"orange.widgets": ("Textable Proto = Textableprototype")},
-    )
-
-##python setup.py develop
